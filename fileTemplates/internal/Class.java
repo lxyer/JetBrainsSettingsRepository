1e348bec2125b1677af54f7a031d186160e562ce
lxyer <lxyer1@gmail.com>
2018-11-12 09:51:37 +0800

    IU-183.4284.36 <lxyer@lxyerdeimac.local Create IntelliLang.xml, print.xml, project.default.xml, diff.xml, yiiguxing.translation.xml, power.mode.ii.xml, Eclipse lxyer.xml, Default.xml, ignore.xml, find.xml, SQL Script.sql, code-navigator.xml, _@user_Material Oceanic.icls, _@user_Darcula.icls, path.macros.xml, javarebel.xml, bootstrap.xml, Default.xml, intellij-javadocs.xml, activate-power-mode_setting.xml, scala.xml, JavaScript.xml, Eclipse copy.xml, Solarized Light (Alternate).xml, Ladies Night 2.xml, editor.codeinsight.xml, grepConsole.xml, keymap.xml, iterations.xml, editor.xml, applicationLibraries.xml, markdown_navigator.shared.xml, debugger.xml, updates.xml, baseRefactoring.xml, vim_settings.xml, Vibrant Unknown (Darcula).icls, ide.general.xml, _@user_Default.icls, _@user_High ontrast.icls, ide-features-trainer.xml, gvim.icls, CodeGlance.xml, rainbow_brackets.xml, lombok-plugin.xml, git.xml, other.xml, ui.lnf.xml, filetypes.xml, javaeeExternalResources.xml, Java.xml, lxyer.icls, highlight-bracket-pair-plugin.xml, plugin_ui.xml, watcherDefaultTasks.xml, colors.scheme.xml, notifications.xml, scala_config.xml, options.xml, databaseDrivers.xml, github.xml, templates.xml, Vue.xml, emmet.xml, output.xml, Relax Your Eyes Green.icls, material_custom_theme.xml, parameter.hints.xml, Class.java, jdk.table.xml, mavenRunHelper.xml, laf.xml, Mac OS X 10_5_ copy.xml, sexyeditor.xml, web-browsers.xml, macros.xml, material_theme.xml, Bootstrap3.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, java.decompiler.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/** 
+ * Created with ${PRODUCT_NAME}. 
+ * @author: ${USER} 
+ * Date: ${DATE} 
+ * Time: ${TIME} 
+ */
+public class ${NAME} {
+}
